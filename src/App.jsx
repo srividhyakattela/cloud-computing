4cf9f86c9f1db6cc2f9ffef3128c01e7f728c8cf
SRIVIDHYA-13 <sri0vidya@gmail.com>
2025-06-25 15:12:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+     <div>
+    <h1> hi srividhya</h1>
+    <h2>I am from lpu</h2>
+   </div>
+  )
+}
+
+export default App
